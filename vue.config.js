64ff6dd57d595c3c81b6500440a3cb0e2cc51a65
allisonkydy <allisnelly@gmail.com>
2020-04-06 16:46:20 -0500

    uncomment devServer config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 // const path = require('path');
 
-// module.exports = {
+module.exports = {
     // outputDir: path.resolve(__dirname, '../server/public'),
-    // devServer: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:5000'
-    //         }
-    //     }
-    // }
-// }
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000'
+            }
+        }
+    }
+}
